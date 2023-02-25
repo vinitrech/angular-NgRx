485e549c1a13ad2f54d4ad78f6c78f9a45725ee7
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-02-25 20:25:59 -0300

    preparing shopping-list actions for updating and deleting ingredients

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import {Ingredient} from "../../shared/ingredient.model";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT'; // convention of using constant which name is the same as the identifier
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action {
     readonly type: string = ADD_INGREDIENT; // readonly is a good practice for type safety
@@ -15,5 +17,19 @@ export class AddIngredients implements Action {
     readonly type: string = ADD_INGREDIENTS;
 
     constructor(public payload: Ingredient[]) {
+    }
+}
+
+export class UpdateIngredient implements Action {
+    readonly type: string = UPDATE_INGREDIENT;
+
+    constructor(public payload: { index: number, ingredient: Ingredient }) {
+    }
+}
+
+export class DeleteIngredient implements Action {
+    readonly type: string = DELETE_INGREDIENT;
+
+    constructor(public payload: number) {
     }
 }
