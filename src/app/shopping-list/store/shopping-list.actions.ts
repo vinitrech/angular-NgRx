633980fb439dbf69eafd13bbbcd25e3d0abfb352
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-03-04 14:07:11 -0300

    adjusting actions names and installing ngrx effects

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,12 @@
 import {Action} from "@ngrx/store";
 import {Ingredient} from "../../shared/ingredient.model";
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT'; // convention of using constant which name is the same as the identifier
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
-export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
-export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+export const ADD_INGREDIENT = '[Shopping List] Add Ingredient'; // convention of using constant with the feature prefix for unique identification
+export const ADD_INGREDIENTS = '[Shopping List] Add Ingredients';
+export const UPDATE_INGREDIENT = '[Shopping List] Update Ingredient';
+export const DELETE_INGREDIENT = '[Shopping List] Delete Ingredient';
+export const START_EDIT = '[Shopping List] Star Edit';
+export const STOP_EDIT = '[Shopping List] Stop Edit';
 
 export class AddIngredient implements Action {
     readonly type: string = ADD_INGREDIENT; // readonly is a good practice for type safety
