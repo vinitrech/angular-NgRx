07d31e3b6369ebe1096e4baaefe764d561d336e0
Vinicius <vinitrech@gmail.com>
2023-03-08 20:25:24 -0300

    adding store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import {StoreModule} from "@ngrx/store";
 import * as fromApp from "./store/app.reducer";
 import {EffectsModule} from "@ngrx/effects";
 import {AuthEffects} from "./auth/store/auth.effects";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../environments/environment"
 
 // There should not be unused imports here, they would be bundled even if not used.
 
@@ -25,6 +27,7 @@ import {AuthEffects} from "./auth/store/auth.effects";
         AppRoutingModule,
         StoreModule.forRoot(fromApp.appReducer), // upon initializing, there will be initial actions dispatched / any action dispatched hits ALL reducers
         EffectsModule.forRoot([AuthEffects]),
+        StoreDevtoolsModule.instrument({logOnly: environment.production}),
         HttpClientModule,
         SharedModule,// Components, directives, pipes work standalone inside modules, so every module needs its own imports. The only exception are services, which can be declared in the AppModule only once, and used application wide
         CoreModule
