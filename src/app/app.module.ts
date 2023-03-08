86727c7b17b84c86df59605ec9c3dbd0251042fc
Vinicius <vinitrech@gmail.com>
2023-03-08 20:35:15 -0300

    adding router store to angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {EffectsModule} from "@ngrx/effects";
 import {AuthEffects} from "./auth/store/auth.effects";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {environment} from "../environments/environment"
+import {StoreRouterConnectingModule} from "@ngrx/router-store";
 
 // There should not be unused imports here, they would be bundled even if not used.
 
@@ -27,7 +28,8 @@ import {environment} from "../environments/environment"
         AppRoutingModule,
         StoreModule.forRoot(fromApp.appReducer), // upon initializing, there will be initial actions dispatched / any action dispatched hits ALL reducers
         EffectsModule.forRoot([AuthEffects]),
-        StoreDevtoolsModule.instrument({logOnly: environment.production}),
+        StoreDevtoolsModule.instrument({logOnly: environment.production}), // Store dev tools to sync with Redux Dev Tools extension for browser, to monitor actions on the go
+        StoreRouterConnectingModule.forRoot(), // Bindings to connect Angular Router with Store
         HttpClientModule,
         SharedModule,// Components, directives, pipes work standalone inside modules, so every module needs its own imports. The only exception are services, which can be declared in the AppModule only once, and used application wide
         CoreModule
