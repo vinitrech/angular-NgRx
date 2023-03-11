a17b65ec29a6270ab5e90fe7de919fd796465203
Vinicius <vinitrech@gmail.com>
2023-03-10 22:04:50 -0300

    fetching recipe detail data

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,34 +2,46 @@ import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../../shared/recipe.model";
 import {RecipesService} from "../../shared/recipesService";
 import {ActivatedRoute, Router} from "@angular/router";
+import * as fromApp from "../../store/app.reducer"
+import {Store} from "@ngrx/store";
+import {map, switchMap} from "rxjs";
 
 @Component({
-  selector: 'app-recipes-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+    selector: 'app-recipes-detail',
+    templateUrl: './recipe-detail.component.html',
+    styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  recipe: Recipe = new Recipe("", "", "", []);
-  id: number = 0;
+    recipe: Recipe = new Recipe("", "", "", []);
+    id: number = 0;
 
-  constructor(private recipeService: RecipesService, private activeRoute: ActivatedRoute, private router: Router) {
-  }
+    constructor(private recipeService: RecipesService, private activeRoute: ActivatedRoute, private router: Router, private store: Store<fromApp.AppState>) {
+    }
 
-  ngOnInit() {
-    // this.recipe = this.recipeService.getRecipe(+this.activeRoute.snapshot.params['id']) not needed if there is a 'subscribe' observable being used for this paramter
+    ngOnInit() {
+        this.activeRoute.params.pipe(map(params => {
+                return +params['id'];
+            }), switchMap((id) => {
+                this.id = id;
+                return this.store.select('recipes');
+            }),
+            map(recipesState => {
+                return recipesState.recipes.find((recipe, index) => {
+                    return index === this.id
+                })
+            })).subscribe(recipe => {
+            if (recipe !== undefined) {
+                this.recipe = recipe;
+            }
+        });
+    }
 
-    this.activeRoute.params.subscribe((params) => {
-      this.recipe = this.recipeService.getRecipe(+params['id'])
-      this.id = +params['id'];
-    })
-  }
+    addToShoppingList() {
+        this.recipeService.addIngredients(this.recipe.ingredients);
+    }
 
-  addToShoppingList() {
-    this.recipeService.addIngredients(this.recipe.ingredients);
-  }
-
-  deleteRecipe() {
-    this.recipeService.deleteRecipe(this.id);
-    this.router.navigate(["../"], {relativeTo: this.activeRoute});
-  }
+    deleteRecipe() {
+        this.recipeService.deleteRecipe(this.id);
+        this.router.navigate(["../"], {relativeTo: this.activeRoute});
+    }
 }
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,95 +2,106 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params, Router} from "@angular/router";
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {RecipesService} from "../../shared/recipesService";
+import * as fromApp from "../../store/app.reducer"
+import {Store} from "@ngrx/store";
+import {map} from "rxjs";
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+    selector: 'app-recipe-edit',
+    templateUrl: './recipe-edit.component.html',
+    styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-  id: number = 0;
-  editMode: boolean = false;
-  recipeForm!: FormGroup;
+    id: number = 0;
+    editMode: boolean = false;
+    recipeForm!: FormGroup;
 
-  constructor(private router: Router, private activeRoute: ActivatedRoute, private recipesService: RecipesService) {
-  }
+    constructor(private router: Router, private activeRoute: ActivatedRoute, private recipesService: RecipesService, private store: Store<fromApp.AppState>) {
+    }
 
-  ngOnInit() {
-    this.activeRoute.params.subscribe((params: Params) => {
-      this.id = +params['id'];
-      this.editMode = params['id'] !== undefined;
-      this.initForm();
-    })
-  }
+    ngOnInit() {
+        this.activeRoute.params.subscribe((params: Params) => {
+            this.id = +params['id'];
+            this.editMode = params['id'] !== undefined;
+            this.initForm();
+        })
+    }
 
-  onAddIngredient() {
-    (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
-      'name': new FormControl(null, Validators.required),
-      'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
-    }))
-  }
+    onAddIngredient() {
+        (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
+            'name': new FormControl(null, Validators.required),
+            'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+        }))
+    }
 
-  private initForm() {
-    let recipeName = '';
-    let recipeImagePath = '';
-    let recipeDescription = '';
-    let recipeIngredients = new FormArray<FormGroup>([]);
+    private initForm() {
+        let recipeName = '';
+        let recipeImagePath = '';
+        let recipeDescription = '';
+        let recipeIngredients = new FormArray<FormGroup>([]);
 
-    if (this.editMode) {
-      const recipe = this.recipesService.getRecipe(this.id);
-      recipeName = recipe.name;
-      recipeImagePath = recipe.imagePath;
-      recipeDescription = recipe.description;
+        if (this.editMode) {
 
-      if (recipe['ingredients']) {
-        for (let ingredient of recipe.ingredients) {
-          recipeIngredients.push(
-            new FormGroup({
-              'name': new FormControl(ingredient.name, Validators.required),
-              'amount': new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+            this.store.select('recipes').pipe(map(recipesState => {
+                return recipesState.recipes.find((recipe, index) => {
+                    return index === this.id;
+                })
+            })).subscribe(recipe => {
+                if (recipe !== undefined) {
+                    recipeName = recipe.name;
+                    recipeImagePath = recipe.imagePath;
+                    recipeDescription = recipe.description;
+
+                    if (recipe['ingredients']) {
+                        for (let ingredient of recipe.ingredients) {
+                            recipeIngredients.push(
+                                new FormGroup({
+                                    'name': new FormControl(ingredient.name, Validators.required),
+                                    'amount': new FormControl(ingredient.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
+                                })
+                            );
+                        }
+                    }
+                }
             })
-          );
         }
-      }
+
+        this.recipeForm = new FormGroup({
+            'name': new FormControl(recipeName, Validators.required),
+            'imagePath': new FormControl(recipeImagePath, Validators.required),
+            'description': new FormControl(recipeDescription, Validators.required),
+            'ingredients': recipeIngredients
+        });
     }
 
-    this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName, Validators.required),
-      'imagePath': new FormControl(recipeImagePath, Validators.required),
-      'description': new FormControl(recipeDescription, Validators.required),
-      'ingredients': recipeIngredients
-    });
-  }
-
-  deleteIngredient(index: number) {
-    // this.recipeForm.value.ingredients.splice(index, 1); <- this won't work, it will delete but won't fire a change to the screen
-    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
-    //(<FormArray>this.recipeForm.get('ingredients')).clear() <- removes all items from array
-  }
-
-  cancelForm() {
-    this.router.navigate(['../'], {relativeTo: this.activeRoute});
-  }
-
-  onSubmit() {
-    // const newRecipe = new Recipe(
-    //   this.recipeForm.value.name,
-    //   this.recipeForm.value.description,
-    //   this.recipeForm.value.imagePath,
-    //   this.recipeForm.value.ingredients
-    // );
-
-    if (this.editMode) {
-      this.recipesService.updateRecipe(this.id, this.recipeForm.value); // the form value is already exactly matching the recipe model
-    } else {
-      this.recipesService.addRecipe(this.recipeForm.value);
+    deleteIngredient(index: number) {
+        // this.recipeForm.value.ingredients.splice(index, 1); <- this won't work, it will delete but won't fire a change to the screen
+        (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+        //(<FormArray>this.recipeForm.get('ingredients')).clear() <- removes all items from array
     }
 
-    this.cancelForm();
-  }
+    cancelForm() {
+        this.router.navigate(['../'], {relativeTo: this.activeRoute});
+    }
 
-  getControls() {
-    return (<FormArray>this.recipeForm.get('ingredients')).controls;
-  }
+    onSubmit() {
+        // const newRecipe = new Recipe(
+        //   this.recipeForm.value.name,
+        //   this.recipeForm.value.description,
+        //   this.recipeForm.value.imagePath,
+        //   this.recipeForm.value.ingredients
+        // );
+
+        if (this.editMode) {
+            this.recipesService.updateRecipe(this.id, this.recipeForm.value); // the form value is already exactly matching the recipe model
+        } else {
+            this.recipesService.addRecipe(this.recipeForm.value);
+        }
+
+        this.cancelForm();
+    }
+
+    getControls() {
+        return (<FormArray>this.recipeForm.get('ingredients')).controls;
+    }
 }
